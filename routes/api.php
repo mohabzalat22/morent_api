213c76fd00abe9c9b7549998cdcd64a5bf0dcee3
mohabzalat22 <mohabzalat22@gmail.com>
2024-05-10 23:54:08 +0300

    breeze api authentication using sanctum csrf.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DetailController;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// AUTH
+require __DIR__.'/auth.php';
 
 // HOME
 Route::post('/', [HomeController::class, 'index']);
