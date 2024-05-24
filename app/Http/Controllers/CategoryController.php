3b5004d5741cb70def26fec5c1c01d2b97c6e683
mohabzalat22 <mohabzalat22@gmail.com>
2024-05-24 20:46:09 +0300

    category filter done.(location,date,time)+left_Side_Filter

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CategoryController extends Controller
 {
@@ -37,6 +38,73 @@ class CategoryController extends Controller
         return $DATA;
     }
     public function filter(Request $request){
+        $req_length = count($request->data);
+        if($req_length > 0)
+        {
+            if($req_length == 1) {
+                // pick or drop
+                //pick
+                if(array_key_exists('pick', $request->data[0])){ 
+                    // start time filter not between database start , end 
+                    $pick_location = strtolower($request->data[0]['pick']['location']);
+
+                    $pick_date_time = carbon::parse($request->data[0]['pick']['date'] . $request->data[0]['pick']['time']);
+                    $cars_after_filter = Car::whereHas(
+                        'reservations',
+                        function ($query) use ($pick_date_time, $pick_location) {
+                            $query->where('start_time', '>', $pick_date_time);
+                            $query->orWhere('end_time', '<', $pick_date_time);
+                            $query->where('pick', $pick_location);
+                        }
+                    )
+                    ->whereIn('model', $request->type)
+                    ->whereIn('capacity', $request->capacity)
+                    ->where('dailyPrice', '<=', $request->price)
+                    ->get();
+                    return $cars_after_filter;
+                }
+                //drop
+                else{
+                    $drop_location = strtolower($request->data[0]['drop']['location']);
+                    $drop_date_time = carbon::parse($request->data[0]['drop']['date'] . $request->data[0]['drop']['time']);
+                    $cars_after_filter = Car::whereHas(
+                        'reservations',
+                        function ($query) use ($drop_date_time, $drop_location) {
+                            $query->where('start_time', '>', $drop_date_time);
+                            $query->orWhere('end_time', '<', $drop_date_time);
+                            $query->where('drop', $drop_location);
+                        }
+                    )
+                    ->whereIn('model', $request->type)
+                    ->whereIn('capacity', $request->capacity)
+                    ->where('dailyPrice', '<=', $request->price)
+                    ->get();
+                    return $cars_after_filter;
+                }
+            }
+            elseif($req_length==2){
+                // both
+                $pick_location = strtolower($request->data[0]['pick']['location']);
+                $drop_location = strtolower($request->data[1]['drop']['location']);
+                $pick_date_time = carbon::parse($request->data[0]['pick']['date'] . $request->data[0]['pick']['time']);
+                $drop_date_time = carbon::parse($request->data[1]['drop']['date'] . $request->data[1]['drop']['time']);
+
+                $cars_after_filter = Car::whereHas(
+                    'reservations',
+                    function ($query) use ($pick_date_time, $drop_date_time, $pick_location, $drop_location) {
+                        $query->where('start_time', '>', $pick_date_time)->where('start_time', '>', $drop_date_time);
+                        $query->orWhere('end_time', '<', $pick_date_time)->where('end_time', '<', $drop_date_time);
+                        $query->where('pick', $pick_location);
+                        $query->where('drop', $drop_location);
+                    }
+                )
+                ->whereIn('model', $request->type)
+                ->whereIn('capacity', $request->capacity)
+                ->where('dailyPrice', '<=', $request->price)
+                ->get();
+                return $cars_after_filter;
+            }
+        }
         // maybe no validation required here
         return car::whereIn('model', $request->type)->whereIn('capacity', $request->capacity)->where('dailyPrice', '<=', $request->price)->get();
     }
